338d897351a9136719ea0901ddc3c61bd4ad7d3a
이영우 <zestlee1106@gmail.com>
2024-07-18 11:29:50 +0900

    add: api 모듈 추가

---
+++ src/api/index.ts
@@ -0,0 +1,45 @@
+// api.js
+const API_BASE_URL = 'http://localhost:3333'
+
+const request = async <T>(endpoint: string, options = {}): Promise<T> => {
+  const url = `${API_BASE_URL}${endpoint}`
+  const response = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...options,
+  })
+
+  if (!response.ok) {
+    const error = await response.json()
+    throw new Error(error.message)
+  }
+
+  return response.json()
+}
+
+export const getData = <T>(endpoint: string): Promise<T> => {
+  return request(endpoint, {
+    method: 'GET',
+  })
+}
+
+export const postData = <T>(endpoint: string, data: Record<string, string>): Promise<T> => {
+  return request(endpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  })
+}
+
+export const putData = <T>(endpoint: string, data: Record<string, string>): Promise<T> => {
+  return request(endpoint, {
+    method: 'PUT',
+    body: JSON.stringify(data),
+  })
+}
+
+export const deleteData = <T>(endpoint: string): Promise<T> => {
+  return request(endpoint, {
+    method: 'DELETE',
+  })
+}
