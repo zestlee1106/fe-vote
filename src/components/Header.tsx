a3c3ec8348dd105da266396119b4fa1c4eddfd35
이영우 <zestlee1106@gmail.com>
2024-07-27 21:32:49 +0900

    update: 헤더 오류 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react'
 import { AppBar, Box, Toolbar, Tabs, Tab } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Header: React.FC = () => {
   const navigate = useNavigate()
   const [value, setValue] = useState(0)
+  const location = useLocation()
+  const pathname = location.pathname
+
+  const tabs = [
+    {
+      name: '투표 목록',
+      path: '/',
+    },
+    {
+      name: '투표 생성',
+      path: '/create-vote',
+    },
+  ]
+
+  const isCurrentTab = pathname
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue)
-    switch (newValue) {
-      case 0:
-        navigate('/')
-        break
-      case 1:
-        navigate('/create-vote')
-        break
-    }
+    navigate(tabs[newValue].path)
   }
 
   return (
@@ -23,8 +31,9 @@ const Header: React.FC = () => {
       <AppBar position="static">
         <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
           <Tabs value={value} onChange={handleChange} aria-label="nav tabs">
-            <Tab label="투표 목록" />
-            <Tab label="투표 생성" />
+            {tabs.map((tab, index) => (
+              <Tab key={index} label={tab.name} />
+            ))}
           </Tabs>
         </Toolbar>
       </AppBar>
--- src/pages/VoteResult.tsx
+++ src/pages/VoteResult.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { Box, Card, CardContent, Typography, List, ListItem, ListItemText, LinearProgress } from '@mui/material'
 import { getVoteResult } from '@/api/votes'
 import { VoteResultResponse } from '@/types/vote'
